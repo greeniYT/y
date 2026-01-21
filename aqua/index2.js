cba7e3784eb953327e7a1be5f9a00998420bb04b
GreeniYT <greenirealz@gmail.com>
2026-01-21 07:06:35 -0500

    Update and rename index.js to index2.js

---
+++ aqua/index2.js
@@ -0,0 +1,6 @@
+window.screenOrientation = "sensor_landscape", 
+loadLib("https://cdn.jsdelivr.net/gh/greeniYT/y@main/aqua/libs/laya.core.js"), 
+loadLib("https://cdn.jsdelivr.net/gh/greeniYT/y@main/aqua/libs/laya.ui.js"), 
+loadLib("https://cdn.jsdelivr.net/gh/greeniYT/y@main/aqua/libs/laya.physics3D.js"), 
+loadLib("https://cdn.jsdelivr.net/gh/greeniYT/y@main/aqua/libs/laya.d3.js"), 
+loadLib("https://cdn.jsdelivr.net/gh/greeniYT/y@main/aqua/js/bundle_.js");
