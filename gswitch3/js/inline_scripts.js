b763d91a2012cc877078728211621a12162620a7
GreeniYT <greenirealz@gmail.com>
2026-01-31 14:12:04 -0500

    Add files via upload

---
+++ gswitch3/js/inline_scripts.js
@@ -0,0 +1,11 @@
+
+		window.addEventListener ("touchmove", function (event) { event.preventDefault (); }, { capture: false, passive: false });
+		if (typeof window.devicePixelRatio != 'undefined' && window.devicePixelRatio > 2) {
+			var meta = document.getElementById ("viewport");
+			meta.setAttribute ('content', 'width=device-width, initial-scale=' + (2 / window.devicePixelRatio) + ', user-scalable=no');
+		}
+	
+
+
+		lime.embed ("G-Switch3", "openfl-content", 0, 0, { parameters: {} });
+	
