3d5a9a3fcd4b747bae06ee633f4be85435494128
GreeniYT <greenirealz@gmail.com>
2026-01-30 20:05:22 -0500

    Add files via upload

---
+++ gswitch/js/inline_scripts.js
@@ -0,0 +1,13 @@
+document.domain='4399.com'
+
+
+		window.addEventListener ("touchmove", function (event) { event.preventDefault (); }, { capture: false, passive: false });
+		if (typeof window.devicePixelRatio != 'undefined' && window.devicePixelRatio > 2) {
+			var meta = document.getElementById ("viewport");
+			meta.setAttribute ('content', 'width=device-width, initial-scale=' + (2 / window.devicePixelRatio) + ', user-scalable=no');
+		}
+	
+
+
+		lime.embed ("G-Switch", "openfl-content", 0, 0, { parameters: {} });
+	
